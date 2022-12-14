64148c9029315612c7c80b5c11ef760ea19df38c
Steven Weaver <sweaver@temple.edu>
2022-12-14 14:55:33 -0600

    add update landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <!-- <link rel="stylesheet" href="https://datamonkey.org/assets/css/datamonkey.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous"> -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Major+Mono+Display">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fontdiner+Swanky">
 </svelte:head>
 
 <script>
@@ -18,7 +19,7 @@
 <style windi:preflights:global>
 
   .header-logo {
-    font-family: "Major Mono Display", monospace;
+    font-family:"Fontdiner Swanky";
     font-size: 20px;
   }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,30 @@
-<h1>Welcome to AUTO-TUNE</h1>
-<ul>
-  <li><a href="china">China</li>
-  <li><a href="combos">Combos</li>
-  <li><a href="lanl">LANL</li>
-  <li><a href="mexico">Mexico</li>
-  <li><a href="papers">Papers</li>
-  <li><a href="singles">Singles</li>
-</ul>
+<style>
+  .container {
+      font-family:"Fontdiner Swanky";
+      font-size : x-large;
+    }
+</style>
+
+<div class="container px-5">
+
+  <div class="grid grid-cols-6 my-5">
+    <div class="col-start-2 col-span-4">
+      <h1 class="text-6xl">✴ Welcome to ✴</h1>
+      <h1 class="text-7xl"> 🎶 AUTO-TUNE 🎶</h1>
+    </div>
+  </div>
+
+  <div class="items-center">
+    <ul class="col-span-6 p-4">
+      <li class="border-gray-400 mb-2"><a href="lanl">LANL</li>
+      <li class="border-gray-400 mb-2"><a href="china">China</li>
+      <!-- <li><a href="combos">Combos</li> -->
+      <li class="border-gray-400 mb-2"><a href="mexico">Mexico</li>
+      <li class="border-gray-400 mb-2"><a href="papers">Papers</li>
+      <!-- <li><a href="singles">Singles</li> -->
+    </ul>
+  </div>
+
+</div>
+
+
