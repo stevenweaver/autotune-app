606bfc4530924e35da76d604f361a64ee77785aa
Steven Weaver <sweaver@temple.edu>
2022-12-14 16:27:44 -0600

    adding landing page bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,28 +3,51 @@
       font-family:"Fontdiner Swanky";
       font-size : x-large;
     }
+
+  .bg-container {
+      background-image: url('./src/img/palmsprings.png');
+      background-repeat: repeat-x;
+      background-size: cover;
+      padding: 0px;
+      margin: 0px;
+      height:100%;
+      width:100%;
+      max-width: 100% !important;
+      max-height: 100% !important;
+      min-height: 100vh !important;
+    }
+
+  .container {
+      opacity: 1.0;
+      min-height: 100vh;
+      max-width: 100% !important;
+      width: 100%;
+  }
+
+
 </style>
 
-<div class="container px-5">
+<div class="bg-container">
+  <div class="container bg-slate-100/40">
 
-  <div class="grid grid-cols-6 my-5">
-    <div class="col-start-2 col-span-4">
-      <h1 class="text-6xl">✴ Welcome to ✴</h1>
-      <h1 class="text-7xl"> 🎶 AUTO-TUNE 🎶</h1>
+    <div class="grid grid-cols-6 py-5">
+      <div class="col-start-2 col-span-4">
+        <h1 class="text-6xl">✴ Welcome to ✴</h1>
+        <h1 class="text-7xl"> 🎶 AUTO-TUNE 🎶</h1>
+      </div>
     </div>
-  </div>
 
-  <div class="items-center">
-    <ul class="col-span-6 p-4">
-      <li class="border-gray-400 mb-2"><a href="lanl">LANL</li>
-      <li class="border-gray-400 mb-2"><a href="china">China</li>
-      <!-- <li><a href="combos">Combos</li> -->
-      <li class="border-gray-400 mb-2"><a href="mexico">Mexico</li>
-      <li class="border-gray-400 mb-2"><a href="papers">Papers</li>
-      <!-- <li><a href="singles">Singles</li> -->
-    </ul>
-  </div>
+    <div class="items-center">
+      <ul class="col-span-6 p-4">
+        <li class="border-gray-400 mb-2"><a href="lanl">LANL</li>
+        <li class="border-gray-400 mb-2"><a href="china">China</li>
+        <!-- <li><a href="combos">Combos</li> -->
+        <li class="border-gray-400 mb-2"><a href="mexico">Mexico</li>
+        <li class="border-gray-400 mb-2"><a href="papers">Papers</li>
+        <!-- <li><a href="singles">Singles</li> -->
+      </ul>
+    </div>
 
+  </div>
 </div>
-
 
