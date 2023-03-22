f6ec03eef4386cd6293aec31bc01511638fa793c
Steven Weaver <sweaver@temple.edu>
2023-03-22 11:45:29 -0600

    adding 100% sampling

--- src/routes/subsample/+page.svelte
+++ src/routes/subsample/+page.svelte
@@ -19,11 +19,14 @@
   let options; 
   let thresholdOptions; 
   let scoreOptions; 
+  let fullSamplingScoreOptions; 
 
   // Get all data to import
-
   let paths = R.flatten(R.map(i => R.map( j =>  { return { "sample": i, "iteration": j, "name": `${i}_${j}`, "path": `../../data/30063225/${i}/${j}/sampled.tn93output.report.tsv?raw` } }, R.range(1,11)), [25,50,75]));
   let dataPromise = R.map(p => import(p['path']), paths);
+
+  import fullSampleValuesRaw from '../../data/30063225/100/sequence.tn93output.report.tsv?raw';
+  let fullSampleValues = d3.tsvParse(fullSampleValuesRaw , d3.autoType);
 
   onMount(async () => {
 
@@ -37,14 +40,11 @@
       dataValues = mapIndexed((items, i) => R.map(item => R.assoc('Iteration', paths[i]['iteration'], item), items), dataValues);
       totalReport = R.flatten(dataValues);
 
-
       let maxItem = R.map(items => items.reduce(function(a, b) { return a.Score >= b.Score ? a : b }, {}), dataValues);
 
       allItems = mapIndexed((item, i) => R.assoc('Name', paths[i]['name'], item), maxItem);
       allItems = mapIndexed((item, i) => R.assoc('Sample', paths[i]['sample'], item), allItems);
       allItems = mapIndexed((item, i) => R.assoc('Iteration', paths[i]['iteration'], item), allItems);
-
-      console.log(allItems);
 
       const thresholdXy = {z: "Sample", x: "Iteration", y: "Threshold"};
 
@@ -57,7 +57,7 @@
           label: "Sample →"
         },
         y: {
-            transform: d=> d*100
+            transform: d => d*100
         },
         color: {
           type: "ordinal",
@@ -68,9 +68,7 @@
         },
         marks: [
           Plot.ruleX([0]),
-          //Plot.ruleY(allItems, Plot.groupY({x1: "min", x2: "max"}, thresholdXy)),
-          Plot.dot(allItems, {...thresholdXy , fill: "Sample"}),
-          //Plot.text(allItems, Plot.selectMinX({...thresholdXy, textAnchor: "end", dx: -6, text: "Iteration"}))
+          Plot.dot(allItems, {...thresholdXy , fill: "Sample"})
         ]
 
       }
@@ -97,7 +95,7 @@
         },
         marks: [
           Plot.ruleX([0]),
-          Plot.dot(allItems, {...xy, fill: "Sample"}),
+          Plot.dot(allItems, {...xy, fill: "Sample"})
         ]
 
       }
@@ -116,6 +114,10 @@
         x: {
           nice: true
         },
+        y: {
+          domain: [-0.5,2],
+          transform: d => R.max(-0.5, d)
+        },
         marks: [
           Plot.frame(),
           Plot.dot(totalReport, {x: "Threshold", y: "Score", fill: d => d.Score, r: 3}),
@@ -128,6 +130,27 @@
       }
 
     });
+
+    fullSamplingScoreOptions = {
+      grid: true,
+      inset: 5,
+      width: 800,
+      height: 300,
+      paddingLeft:250,
+      x: {
+        nice: true
+      },
+      marks: [
+        Plot.frame(),
+        Plot.dot(fullSampleValues, {x: "Threshold", y: "Score", fill: d => d.Score, r: 3}),
+      ],
+      color: {
+        legend: true, 
+        label: "Threshold score",
+        type: "symlog"
+      }
+    }
+
 
 });
 
@@ -171,6 +194,17 @@
       <p class="py-5"> Figure 2. This figure shows a plot with years on the x-axis and a heuristic score on the y-axis. The heuristic score is based on the number of clusters and the ratio of the largest cluster to the second largest cluster. The points on the scatter plot are colored by proportion of random samples from original dataset. Each point represents the year and the corresponding heuristic score for each respective sample. </p>
 
     </div>
+
+    <div class="full-scores">
+
+      <h1 class="text-xl py-2">Full Sampling Scores</h1>
+
+      <h3>Plot</h3>
+      <RenderPlot options={fullSamplingScoreOptions} />
+      <p class="py-5"> Figure 2. This figure shows a plot with years on the x-axis and a heuristic score on the y-axis. The heuristic score is based on the number of clusters and the ratio of the largest cluster to the second largest cluster. The points on the scatter plot are colored by proportion of random samples from original dataset. Each point represents the year and the corresponding heuristic score for each respective sample. </p>
+
+    </div>
+
 
 
     <h3 class="py-2">Table</h3>
