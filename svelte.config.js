3e5b1ecfc6f7773677f7620c16874add2606e102
Steven Weaver <sweaver@temple.edu>
2023-09-18 13:05:59 -0600

    Revert "build updates"
    This reverts commit 3d2d973f35d328f2897ea380987181e125896783.

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 import { windi } from 'svelte-windicss-preprocess';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +11,6 @@ const config = {
     appDir:'static',
 		adapter: adapter({
       			out: 'build',
-      			assets: 'build',
       			precompress: false,
       			envPrefix: 'AUTOTUNE_',
       			polyfill: true
